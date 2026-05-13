1c1cfc0578484cf39f940cb3f2ebd3a61be157cb
Geethikasaiganugapati <ganugapati.saigeethika@gmail.com>
2026-05-13 15:22:34 +0530

    Create InventoryPage.java

---
+++ InventoryPage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class InventoryPage {
+
+    WebDriver driver;
+
+    By inventoryTitle = By.className("title");
+
+    public InventoryPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getPageTitle() {
+        return driver.findElement(inventoryTitle).getText();
+    }
+}
